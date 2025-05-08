c121a2eff43268ec83547be6fd6417a13afbc41e
[Shubham Pandey] <shubhamrajpandey875@gmail.com>
2025-05-08 19:37:13 +0545

    Updated Contact Page

--- contact.php
+++ contact.php
@@ -64,17 +64,39 @@ if (isset($_POST['send'])) {
 
    <section class="contact">
 
-      <form action="" method="post">
-         <h3>Get in touch.</h3>
-         <input type="text" name="name" placeholder="enter your name" required maxlength="20" class="box">
-         <input type="email" name="email" placeholder="enter your email" required maxlength="50" class="box">
-         <input type="number" name="number" min="0" max="9999999999" placeholder="enter your number" required onkeypress="if(this.value.length == 10) return false;" class="box">
-         <textarea name="msg" class="box" placeholder="enter your message" cols="30" rows="10"></textarea>
-         <input type="submit" value="send message" name="send" class="btn">
-      </form>
+   <!-- Left Contact Info Section -->
+   <div class="contact-info">
+      <div class="info-box">
+         <i class="fas fa-phone"></i>
+         <div>
+            <h4>Call To Us</h4>
+            <p>We are available 24/7, 7 days a week.<br>Phone: +977 9823696701</p>
+         </div>
+      </div>
+      <hr>
+      <div class="info-box">
+         <i class="fas fa-envelope"></i>
+         <div>
+            <h4>Write To Us</h4>
+            <p>Fill out our form and we will contact you within 24 hours.<br><br>
+               Emails: customer@shopper.com<br>
+               support@shopper.com
+            </p>
+         </div>
+      </div>
+   </div>
 
-   </section>
+   <form action="" method="post">
+      <div style="display: flex; flex-wrap: wrap; gap: 1rem;">
+         <input type="text" name="name" placeholder="Your Name *" required maxlength="20" class="box" style="flex: 1 1 30%;">
+         <input type="email" name="email" placeholder="Your Email *" required maxlength="50" class="box" style="flex: 1 1 30%;">
+         <input type="number" name="number" min="0" max="9999999999" placeholder="Your Phone *" required onkeypress="if(this.value.length == 10) return false;" class="box" style="flex: 1 1 30%;">
+      </div>
+      <textarea name="msg" class="box" placeholder="Your Message" required></textarea>
+      <input type="submit" value="Send Message" name="send" class="btn">
+   </form>
 
+</section>
    <?php include 'components/footer.php'; ?>
 
    <script src="js/script.js"></script>
